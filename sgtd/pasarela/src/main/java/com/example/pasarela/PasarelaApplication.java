52c3b5e8d691db95b00db1c259b2af51179dd7a9
marko4510 <115999426+marko4510@users.noreply.github.com>
2023-04-26 15:49:07 -0400

    Add files via upload

---
+++ sgtd/pasarela/src/main/java/com/example/pasarela/PasarelaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pasarela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasarelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasarelaApplication.class, args);
+	}
+
+}
---
+++ sgtd/pasarela/src/test/java/com/example/pasarela/PasarelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pasarela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasarelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
