9c0a2211891c0d79fb19dd3fabc9b701d1c35e17
marko4510 <marcocuellar7293@gmail.com>
2023-05-02 11:04:57 -0400

    asdsd

---
+++ sgtd/pasarela/src/main/java/com/example/pasarela/Controller/PersonaControllers/GradoAcademicoController.java
@@ -0,0 +1,94 @@
+package com.example.pasarela.Controller.PersonaControllers;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.pasarela.Models.Entity.GradoAcademico;
+import com.example.pasarela.Models.Service.ICarreraService;
+import com.example.pasarela.Models.Service.IGradoAcademicoService;
+
+
+
+
+
+@Controller
+public class GradoAcademicoController {
+    @Autowired
+    private IGradoAcademicoService gradoAcademicoService;
+
+    @Autowired
+    private ICarreraService carreraService;
+
+    //FUNCION PARA LA VISUALIZACION DE Grado Academico
+	@RequestMapping(value = "/gradoAcademicoR", method = RequestMethod.GET) // Pagina principal
+	public String GradoAca(HttpServletRequest request, Model model) {
+        if (request.getSession().getAttribute("usuario") != null) {
+			
+			model.addAttribute("gradoAcademico", new GradoAcademico());
+            model.addAttribute("gradosAcademicos", gradoAcademicoService.findAll());
+			model.addAttribute("carreras", carreraService.findAll());
+			
+			return "persona/gestionarGradoAcademico";
+		}else{
+			return "redirect:LoginR";
+		}
+	}
+
+    //FUNCION PARA GUARDAR El Grado Academico
+	@RequestMapping(value = "/gradoAcademicoF", method = RequestMethod.POST) // Enviar datos de Registro a Lista
+	public String gradoAcademicoF(@Validated GradoAcademico gradoAcademico) { //validar los datos capturados (1)
+		
+        gradoAcademico.setEstado("A");
+        gradoAcademicoService.save(gradoAcademico);
+		
+		return "redirect:/gradoAcademicoR";
+	}
+
+
+
+    //FUNCION PARA EDITAR EL REGISTRO DE departamento
+	@RequestMapping(value = "/editar-gradoAca/{id_grado_academico}")
+    public String editar_p(@PathVariable("id_grado_academico")Long id_grado_academico, Model model){
+          
+        GradoAcademico gradoAcademico  = gradoAcademicoService.findOne(id_grado_academico);
+
+        model.addAttribute("gradoAcademico", gradoAcademico);
+        model.addAttribute("gradosAcademicos", gradoAcademicoService.findAll());
+		//model.addAttribute("nacionalidades", nacionalidadService.findAll());
+ 
+          
+        return "persona/gestionarGradoAcademico";
+          
+    }
+
+     //FUNCION PARA GUARDAR El Grado Academico
+	@RequestMapping(value = "/gradoAcademicoModF", method = RequestMethod.POST) // Enviar datos de Registro a Lista
+	public String gradoAcademicoModF(@Validated GradoAcademico gradoAcademico) { //validar los datos capturados (1)
+		
+        gradoAcademico.setEstado("A");
+        gradoAcademicoService.save(gradoAcademico);
+		
+		return "redirect:/gradoAcademicoR";
+	}
+
+
+    //FUNCION PARA ELIMINAR EL REGISTRO DE Grado Academico
+	@RequestMapping(value = "/eliminar-gradoAca/{id_grado_academico}")
+	public String eliminar_p(@PathVariable("id_grado_academico")Long id_grado_academico) {
+						
+		
+		GradoAcademico gradoAcademico  = gradoAcademicoService.findOne(id_grado_academico);
+		
+		gradoAcademico.setEstado("X");
+		
+		gradoAcademicoService.save(gradoAcademico);
+		return "redirect:/gradoAcademicoR";
+		
+	}
+}
--- sgtd/pasarela/src/main/java/com/example/pasarela/Controller/SolicitudControllers/SolicitudLegalizacionController.java
+++ sgtd/pasarela/src/main/java/com/example/pasarela/Controller/SolicitudControllers/SolicitudLegalizacionController.java
@@ -1,5 +1,71 @@
 package com.example.pasarela.Controller.SolicitudControllers;
 
+import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.pasarela.Models.Entity.SolicitudLegalizacion;
+import com.example.pasarela.Models.Service.IDepartamentoService;
+import com.example.pasarela.Models.Service.ISolicitudLegalizacionService;
+import com.example.pasarela.Models.Service.IUsuarioService;
+
+@Controller
 public class SolicitudLegalizacionController {
-    
+
+    @Autowired
+    private IDepartamentoService departamentoService;
+
+    @Autowired
+    private ISolicitudLegalizacionService solicitudService;
+
+    @Autowired
+    private IUsuarioService usuarioService;
+
+    // Listar los tipos de documentos
+    @RequestMapping(value = "/SolicitudesLegalizaciones", method = RequestMethod.GET) // Pagina principal
+    public String facultadL(Model model, HttpServletRequest request) {
+        if (request.getSession().getAttribute("usuario") != null) {
+            // model.addAttribute("tipoDocumentos", tipoDocumentoService.findAll());
+
+            model.addAttribute("solicitudes", solicitudService.findAll());
+
+            return "solicitud/gestionarSolicitudLegalizacion";
+        } else {
+            return "redirect:LoginR";
+        }
+    }
+
+    @RequestMapping(value = "/aprobar-solicitud-legalizacion/{id_solicitud_legalizacion}")
+    public String eliminar_p(@PathVariable("id_solicitud_legalizacion") Long id_solicitud_legalizacion) {
+
+        SolicitudLegalizacion solicitud = solicitudService.findOne(id_solicitud_legalizacion);
+
+        solicitud.setEstado("Aprobado");
+
+        solicitudService.save(solicitud);
+        return "redirect:/SolicitudesLegalizaciones";
+
+    }
+
+    @RequestMapping(value = "/rechazar-solicitud-legalizacion/{id_solicitud_legalizacion}")
+    public String rechazar_p(@PathVariable("id_solicitud_legalizacion") Long id_solicitud_legalizacion) {
+
+        SolicitudLegalizacion solicitud = solicitudService.findOne(id_solicitud_legalizacion);
+
+        solicitud.setEstado("Rechazado");
+
+        solicitudService.save(solicitud);
+        return "redirect:/SolicitudesLegalizaciones";
+
+    }
 }
