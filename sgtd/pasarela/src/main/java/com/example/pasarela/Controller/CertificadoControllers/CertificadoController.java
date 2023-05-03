c99ba7ebb498f09133e4c35f3ad2f2df8b72cc8e
marko4510 <marcocuellar7293@gmail.com>
2023-05-03 17:24:12 -0400

    asdasd

---
+++ sgtd/pasarela/src/main/java/com/example/pasarela/Controller/CertificadoControllers/CertificadoController.java
@@ -0,0 +1,37 @@
+package com.example.pasarela.Controller.CertificadoControllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+import com.example.pasarela.Models.Entity.Persona;
+import com.example.pasarela.Models.Service.IPersonaService;
+
+
+
+@Controller
+public class CertificadoController {
+    
+    @Autowired
+    private IPersonaService personaService;
+
+    @GetMapping("/inicioGenerarCertificado")
+    public String inicioGenerarCertificado( Model model) {
+    	model.addAttribute("personas", personaService.findAll());
+    
+        return "certificado/gestionarCertificado";
+    }
+
+    @PostMapping("/generarCertificado")
+    public String generarCertificado(@RequestParam("id_persona") Long id_persona, Model model) {
+    	
+    	Persona persona = personaService.findOne(id_persona);
+    	model.addAttribute("persona", persona);
+    
+        return "certificado/generarCertificado";
+    }
+}
