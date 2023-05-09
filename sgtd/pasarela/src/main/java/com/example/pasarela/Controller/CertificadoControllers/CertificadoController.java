95c776af5de125ff6f332049183ea8ad93b8852b
marko4510 <marcocuellar7293@gmail.com>
2023-05-09 09:37:54 -0400

    qwertyu

--- sgtd/pasarela/src/main/java/com/example/pasarela/Controller/CertificadoControllers/CertificadoController.java
+++ sgtd/pasarela/src/main/java/com/example/pasarela/Controller/CertificadoControllers/CertificadoController.java
@@ -1,5 +1,13 @@
 package com.example.pasarela.Controller.CertificadoControllers;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,12 +35,23 @@ public class CertificadoController {
     }
 
     @PostMapping("/generarCertificadoSupletorio")
-    public String generarCertificadoSupletorio(@RequestParam("id_persona") Long id_persona, Model model) {
-    	
+    public String generarCertificadoSupletorio(@RequestParam("id_persona") Long id_persona,
+    @RequestParam("tipoSerie") String tipoSerie,
+    @RequestParam("nroTitulo") String nroTitulo,
+    Model model)throws FileNotFoundException, IOException, ParseException {
+
+    	   // Capturar Fecha de Registro de SolicitudLegalizacion
+           Date fechaActual = new Date();
+           SimpleDateFormat formato = new SimpleDateFormat("d ' de ' ' ' MMMM ' del ' ' ' yyyy", new Locale("es", "ES"));
+           String fechaTexto = formato.format(fechaActual);
+
     	Persona persona = personaService.findOne(id_persona);
     	model.addAttribute("persona", persona);
-    
-        return "certificado/prueba";
+        model.addAttribute("fechaTitulo", fechaTexto);
+        model.addAttribute("tipoSerie", tipoSerie);
+        model.addAttribute("nroTitulo", nroTitulo);
+
+        return "certificado/plantillaSupleAcademico";
     }
 
     @PostMapping("/generarTituloProvisionNacional")
@@ -45,11 +64,21 @@ public class CertificadoController {
     }
 
     @PostMapping("/generarTitulo")
-    public String generarTitulo(@RequestParam("id_persona") Long id_persona, Model model) {
+    public String generarTitulo(@RequestParam("id_persona") Long id_persona,
+    @RequestParam("tipoSerie") String tipoSerie,
+    @RequestParam("nroTitulo") String nroTitulo,
+    Model model) throws FileNotFoundException, IOException, ParseException{
     	
+         // Capturar Fecha de Registro de SolicitudLegalizacion
+         Date fechaActual = new Date();
+         SimpleDateFormat formato = new SimpleDateFormat("d ' de ' ' ' MMMM ' del ' ' ' yyyy", new Locale("es", "ES"));
+         String fechaTexto = formato.format(fechaActual);
+
     	Persona persona = personaService.findOne(id_persona);
     	model.addAttribute("persona", persona);
-    
-        return "certificado/plantillaSupleAcademico";
+        model.addAttribute("fechaTitulo", fechaTexto);
+        model.addAttribute("tipoSerie", tipoSerie);
+        model.addAttribute("nroTitulo", nroTitulo);
+        return "certificado/plantillaTituloAcademico";
     }
 }
